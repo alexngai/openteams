690556e8a37647f88d2e2cb20db05a13655e3b99
Alex Ngai <alexander.s.ngai@gmail.com>
2026-02-06 23:24:34 -0800

    basic package structure

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export const VERSION = "0.0.1";
+
+export interface OpenTeamsConfig {
+  // placeholder
+}
+
+export function createTeam(config?: OpenTeamsConfig): void {
+  // placeholder
+  console.log("openteams", VERSION);
+}
